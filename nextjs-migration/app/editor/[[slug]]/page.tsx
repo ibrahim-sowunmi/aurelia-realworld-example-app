2dc9b404c768accae3ae0ae63a7cbbfe98d47887
Ibrahim Sowunmi <i.sowunmi97@gmail.com>
2025-10-26 18:32:20 +0000

    feat: implement auth protection with middleware and protected route component
    Co-Authored-By: Ibs <i.sowunmi97@gmail.com>

--- nextjs-migration/app/editor/[[slug]]/page.tsx
+++ nextjs-migration/app/editor/[[slug]]/page.tsx
@@ -1,3 +1,21 @@
+"use client";
+
+import ProtectedRoute from "../../../components/ProtectedRoute";
+
 export default function EditorPage() {
-  return null;
+  return (
+    <ProtectedRoute>
+      {/* Editor page content will go here */}
+      <div className="editor-page">
+        <div className="container page">
+          <div className="row">
+            <div className="col-md-10 offset-md-1 col-xs-12">
+              <h1 className="text-xs-center">Editor</h1>
+              {/* Editor form will be implemented in the next step */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </ProtectedRoute>
+  );
 }
--- nextjs-migration/app/settings/page.tsx
+++ nextjs-migration/app/settings/page.tsx
@@ -1,3 +1,21 @@
+"use client";
+
+import ProtectedRoute from "../../components/ProtectedRoute";
+
 export default function SettingsPage() {
-  return null;
+  return (
+    <ProtectedRoute>
+      {/* Settings page content will go here */}
+      <div className="settings-page">
+        <div className="container page">
+          <div className="row">
+            <div className="col-md-6 offset-md-3 col-xs-12">
+              <h1 className="text-xs-center">Your Settings</h1>
+              {/* Settings form will be implemented in the next step */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </ProtectedRoute>
+  );
 }
