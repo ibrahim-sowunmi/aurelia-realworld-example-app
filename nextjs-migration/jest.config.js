41458e9031fdc476feb12d7eec69f99deba2fc4e
Ibrahim Sowunmi <i.sowunmi97@gmail.com>
2025-10-26 14:46:26 +0000

    Add simplified Jest configuration for testing
    Co-Authored-By: Ibs <i.sowunmi97@gmail.com>

---
+++ nextjs-migration/jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/lib/(.*)$': '<rootDir>/lib/$1',
+    '^@/contexts/(.*)$': '<rootDir>/contexts/$1',
+    '^@/components/(.*)$': '<rootDir>/app/components/$1',
+    '^@/types/(.*)$': '<rootDir>/types/$1'
+  },
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/.next/'
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest']
+  }
+};
